2dc6322de6eb7b25550f4695469c69b9f231ae1f
stvn1 <esteban17100@gmail.com>
2020-04-12 18:26:26 -0400

    Finished

--- server.js
+++ server.js
@@ -19,7 +19,27 @@ io.sockets.on("connection", function (socket) {
 
   //Disconnect
   socket.on("disconnect", function (data) {
+    // if (!socket.username) return;
+    users.splice(users.indexOf(socket.username), 1);
+    updateUsernames();
     connections.splice(connections.indexOf(socket), 1);
-    console.log("Disconnected:  % sockets connected ", connections.length);
+    console.log("Disconnected:  %s sockets connected ", connections.length);
   });
+
+  //Send Message
+  socket.on("send message", function (data) {
+    // console.log(data);
+    io.sockets.emit("new message", { msg: data, user: socket.username });
+  });
+  //New User
+  socket.on("new user", function (data, callback) {
+    console.log(data);
+    callback(true);
+    socket.username = data;
+    users.push(socket.username);
+    updateUsernames();
+  });
+  function updateUsernames() {
+    io.sockets.emit("get users", users);
+  }
 });
